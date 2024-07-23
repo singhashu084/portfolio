90be68ad1b51ca60dbd6279cc9b3bb6650eaae7f
Ashutosh <singhashu084@gmail.com>
2024-07-23 23:55:00 +0530

    initial

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,102 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node JS</li>
+        <li>Nest JS</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>MongoDB</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>React JS</li>
+        <li>Next JS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Engineering from R.G.P.V. Bhopal (M.P.)</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>AWS Cloud Practitioner</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          As a Full Stack Developer with over two years of professional experience, I specialize in building scalable and efficient web applications using a variety of modern technologies. I am passionate about creating seamless and efficient user experiences through robust and maintainable code.
+
+My comprehensive understanding of both frontend and backend technologies allows me to develop full-featured applications that meet diverse client needs. I am committed to continuous learning, always staying updated with the latest industry trends and advancements to bring the best practices and tools to my projects.
+
+Collaboration and teamwork are fundamental to my work ethic. I thrive in dynamic environments, working closely with cross-functional teams to deliver innovative solutions that exceed user expectations.
+
+I am eager to connect with like-minded professionals and explore opportunities to apply my skills in driving innovation and achieving impactful results.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
